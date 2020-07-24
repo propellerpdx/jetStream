1eee4b033b17f68a97fcc232d394351094d2a1a5
Mark Druffel <mark.druffel@gmail.com>
2020-07-24 08:32:06 -0700

    dag tid can't use /

--- R/run_etl_task.R
+++ R/run_etl_task.R
@@ -29,6 +29,8 @@ run_etl_task <- function(dagid = NULL,
                          libs = NULL,
                          verbose = FALSE){
   # Call env_vars -----------------------------------------------------------
+  #TODO move to seaduck
+  tid <- stringr::str_replace(tid = '-','/')
   if(is.null(env_vars) == FALSE){
     if(verbose == TRUE){
       message(glue::glue('jetStream::run_call(call = {env_vars}, libs = {libs}, verbose = {verbose})'))
